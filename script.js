96d552aec2cbff39c856e52a9e6d05eaba57c3f9
Bhiekeys <tinuolajisodimu@gmail.com>
2023-03-06 13:55:57 +0100

    updates

---
+++ script.js
@@ -0,0 +1,76 @@
+// console.log(input.value);
+const oneHandler = () => {
+    input.value += one.innerHTML
+}
+
+const twoHandler = () => {
+    input.value += two.innerHTML
+}
+
+const threeHandler = () => {
+   input.value += three.innerHTML
+}
+
+const fourHandler = () => {
+   input.value += four.innerHTML
+}
+
+const fiveHandler = () => {
+   input.value += five.innerHTML
+}
+
+const sixHandler = () => {
+   input.value += six.innerHTML
+}
+
+const sevenHandler = () => {
+    input.value += seven.innerHTML
+}
+
+const eightHandler = () => {
+    input.value += eight.innerHTML
+}
+
+const nineHandler = () => {
+    input,value += nine.innerHTML
+}
+
+const zeroHandler = () => {
+   input.value += zero.innerHTML
+}
+
+const zerosHandler = () => {
+   input.value += zeros.innerHTML
+}
+
+const plusHandler = () => {
+   input.value += plus.innerHTML
+}
+
+const minusHandler = () => {
+    input.value += minus.innerHTML
+}
+
+const multiplyHandler = () => {
+   input.value += multiply.innerHTML
+}
+
+const equalHandler = () => {
+    input.value = eval(input.value)
+}
+
+const divideHandler = () => {
+   input.value += divide.innerHTML
+}
+
+const dotHandler = () => {
+   input.value += dot.innerHTML
+}
+
+const acHandler = () => {
+    input.value = " "
+}
+
+const delHandler = () => {
+    input.value = result.innerHTML.slice(0, -1)
+}
